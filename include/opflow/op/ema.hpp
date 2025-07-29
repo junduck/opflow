4be81c433bd1fc5ac621e67ae6460d747bfa8592
Jun <imlijunda@gmail.com>
2025-07-29 23:38:29 +0930

    more init strat for EMA

--- include/opflow/op/ema.hpp
+++ include/opflow/op/ema.hpp
@@ -4,7 +4,26 @@
 #include "opflow/op/detail/unary.hpp"
 
 namespace opflow::op {
-template <typename T, std::floating_point U, bool ZeroInit = false>
+
+/**
+ * @brief Enumeration for EMA initialisation strategies
+ *
+ * @details The behaviour of the EMA can be controlled by the initialisation strategy:
+ *
+ * - ema_init::first: Initialise with the first value
+ * - ema_init::zero: Initialise with zero
+ * - ema_init::epsilon: Initialise with a small value (epsilon), useful for avoiding zero division
+ * - ema_init::unbiased: Initialise with bias-corrected value x / (1 - a ^ n) and all produced values are bias-corrected
+ *
+ */
+enum class ema_init {
+  first,   ///< Initialise with the first value
+  zero,    ///< Initialise with zero
+  epsilon, ///< Initialise with a small value (epsilon), useful for avoiding zero division
+  unbiased ///< Initialise with bias-corrected value (1 - a ^ n)
+};
+
+template <typename T, std::floating_point U, ema_init Init = ema_init::first>
 struct ema : public detail::unary_op<T, U> {
   using base = detail::unary_op<T, U>;
   using base::pos;
@@ -13,19 +32,16 @@ struct ema : public detail::unary_op<T, U> {
   U alpha;          ///< Smoothing factor
   bool initialised; ///< Whether the first value has been processed
 
-  explicit ema(U alpha, size_t pos = 0) noexcept : base{pos}, val{}, alpha{alpha}, initialised{ZeroInit} {
+  explicit ema(U alpha, size_t pos = 0) noexcept
+      : base{pos}, val{}, alpha{detail::smooth_factor(alpha)}, initialised{false} {
     assert(alpha > 0. && "alpha/period must be positive.");
-    if (alpha >= 1.) {
-      // alpha is actually a period
-      this->alpha = 2.0 / (alpha + 1);
-    }
   }
 
   void init(T, U const *const *in) noexcept override {
     assert(in && in[0] && "NULL input data.");
 
     val = 0.;
-    initialised = ZeroInit;
+    initialised = false;
     step(T{}, in); // Initialize with the first value
   }
 
@@ -33,19 +49,62 @@ struct ema : public detail::unary_op<T, U> {
     assert(in && in[0] && "NULL input data.");
     U x = in[0][pos];
 
-    if constexpr (!ZeroInit) {
-      if (!initialised) [[unlikely]] {
+    if (!initialised) {
+      if constexpr (Init == ema_init::first) {
         val = x; // Initialize with the first value
-        initialised = true;
-        return;
+      } else if constexpr (Init == ema_init::zero) {
+        val = 0.; // Initialize with zero
+      } else {
+        // Init == ema_init::epsilon
+        val = detail::smallest<U>(); // Initialize with epsilon
       }
+      initialised = true;
+      // unbiased is specialisation
+    } else {
+      val.add(x, alpha);
     }
-    val.add(x, alpha);
   }
 
   void value(U *out) noexcept override {
     assert(out && "NULL output buffer.");
     *out = val;
+  }
+};
+
+// Ref: https://arxiv.org/abs/1412.6980
+
+template <typename T, std::floating_point U>
+struct ema<T, U, ema_init::unbiased> : public detail::unary_op<T, U> {
+  using base = detail::unary_op<T, U>;
+  using base::pos;
+
+  detail::smooth<U> val; ///< Uncorrected EMA value
+  U alpha;               ///< Smoothing factor
+  U bias;                ///< Bias correction factor
+
+  explicit ema(U alpha, size_t pos = 0) noexcept : base(pos), val(), alpha(detail::smooth_factor(alpha)), bias(1.) {
+    assert(alpha > 0. && "alpha/period must be positive.");
+  }
+
+  void init(T, U const *const *in) noexcept override {
+    assert(in && in[0] && "NULL input data.");
+
+    val = 0.;
+    bias = 1.;
+    step(T{}, in); // Initialize with the first value
+  }
+
+  void step(T, U const *const *in) noexcept override {
+    assert(in && in[0] && "NULL input data.");
+    U x = in[0][pos];
+
+    val.add(x, alpha);
+    bias *= alpha;
+  }
+
+  void value(U *out) noexcept override {
+    assert(out && "NULL output buffer.");
+    *out = val / (U(1) - bias); // Apply bias correction
   }
 };
 } // namespace opflow::op
