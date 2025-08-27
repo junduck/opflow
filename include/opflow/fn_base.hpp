c2aaaa246d6a7b852764fa408e5c98c4556e65cc
Jun <imlijunda@gmail.com>
2025-08-27 13:45:41 +0930

    updated fn_base to new interface

--- include/opflow/fn_base.hpp
+++ include/opflow/fn_base.hpp
@@ -2,6 +2,9 @@
 
 #include <cstddef>
 
+#include "common.hpp"
+#include "def.hpp"
+
 namespace opflow {
 /**
  * @brief Base class for online functors
@@ -33,6 +36,39 @@ struct fn_base {
 
   fn_base const *observer() const noexcept { return this; }
 
+  virtual fn_base *clone_at(void *mem) const noexcept = 0;
+  virtual size_t clone_size() const noexcept = 0;
+  virtual size_t clone_align() const noexcept = 0;
+
   virtual ~fn_base() noexcept = default;
+};
+
+template <typename T>
+struct fn_root : fn_base<T> {
+  using base = fn_base<T>;
+  using typename base::data_type;
+
+  size_t const input_size;
+
+  explicit fn_root(size_t n) : input_size(n) {}
+
+  void on_data(data_type const *in, data_type *out) noexcept override {
+    data_type *OPFLOW_RESTRICT cast = out;
+    for (size_t i = 0; i < input_size; ++i) {
+      cast[i] = in[i];
+    }
+  }
+
+  size_t num_inputs() const noexcept override { return input_size; }
+  size_t num_outputs() const noexcept override { return input_size; }
+
+  OPFLOW_CLONEABLE(fn_root)
+};
+
+static_assert(dag_node<fn_root<int>>);
+
+template <typename T>
+struct dag_root<fn_base<T>> {
+  using type = fn_root<T>;
 };
 } // namespace opflow
